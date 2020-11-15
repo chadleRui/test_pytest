18c337f032e704253ee073159c5b546c3e66e3bb
wohouhou <515236901@qq.com>
2020-11-15 14:32:32 +0800

    appium 实现企业微信app  PO 实现添加联系人和删除联系人

--- test_appium/test_app_po/page/base_page.py
+++ test_appium/test_app_po/page/base_page.py
@@ -1,18 +1,33 @@
+import logging
+
 from appium.webdriver.common.mobileby import MobileBy
 from selenium.webdriver.remote.webdriver import WebDriver
 
 
 class BasePage():
+    logging.basicConfig(level=logging.INFO,
+                        format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
+                        datefmt='%a, %d %b %Y %H:%M:%S',
+                        filename='../log/myapp.log',
+                        filemode='w')
     def __init__(self,driver:WebDriver=None):
         self.driver=driver
 
     def find(self,by,locator):
+        logging.info("find:")
+        logging.info(by)
+        logging.info(locator)
         return self.driver.find_element(by,locator)
 
     def finds(self,by,locator):
+        logging.info("finds:")
+        logging.info(by)
+        logging.info(locator)
         return self.driver.find_elements(by,locator)
 
     def find_by_scroll(self,text):
+        logging.info("find_by_scroll")
+        logging.info(text)
         return self.driver.find_element(MobileBy.ANDROID_UIAUTOMATOR,
                                  'new UiScrollable(new UiSelector()'
                                  '.scrollable(true).instance(0))'
